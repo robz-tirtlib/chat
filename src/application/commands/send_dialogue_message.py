ff28425b8a9db6e41958f5d146971b4c00e414f7
robz-tirtlib <62515024+robz-tirtlib@users.noreply.github.com>
2024-02-16 14:47:23 +0300

    Add send dialogue message command

---
+++ src/application/commands/send_dialogue_message.py
@@ -0,0 +1,29 @@
+# Interactor
+# No Depends (to easily test). Just construct Interactor manually in
+# controller (request session or/and repo via Depends and inject them)
+
+from dataclasses import dataclass
+
+from src.application.interfaces.persistence.dialogue_repo import IDialogueRepo
+
+
+@dataclass
+class SendDialogueMessageDTO:
+    dialogue_id: str
+    sender_id: str
+    message_text: str
+
+
+class SendDialogueMessage:
+    def __init__(self, dialogue_repo: IDialogueRepo) -> None:
+        self._dialogue_repo = dialogue_repo
+
+    async def __call__(self, data: SendDialogueMessageDTO) -> str:
+        # TODO: Prolly add smth like user privacy settings to restrict
+        # message sending
+
+        message_id = await self._dialogue_repo.add_message(
+            data.dialogue_id, data.sender_id, data.message_text,
+        )
+
+        return message_id
--- src/application/interfaces/persistence/dialogue_repo.py
+++ src/application/interfaces/persistence/dialogue_repo.py
@@ -5,3 +5,9 @@ class IDialogueRepo(ABC):
     @abstractmethod
     async def add_dialogue(self, user1_id: str, user2_id: str) -> str:
         ...
+
+    @abstractmethod
+    async def add_message(
+            self, dialogue_id: str, sender_id: str, message_text: str,
+    ) -> str:
+        ...
